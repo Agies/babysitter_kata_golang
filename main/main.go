fe574ef016816822402f54023026cc436c1feee1
Jerel R Hass <agies1@hotmail.com>
2015-05-12 00:18:15 -0400

    assuming casing is an issue

--- main/main.go
+++ main/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "github.com/agies/babysitter_kata_golang/calculator"
+import "github.com/Agies/babysitter_kata_golang/calculator"
 
 func main() {
 	result := calculator.Calculate()
