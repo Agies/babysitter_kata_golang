8a305a7fad46503ddf08a297084c4e731bb4b0b2
Jerel R Hass <agies1@hotmail.com>
2015-05-12 23:39:17 -0400

    verify required leavetime

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,37 +6,31 @@ import (
 )
 
 func TestCalculate(t *testing.T) {
-	result, error := Calculate("05/11/2015 5:00pm", "05/11/2015 05:00pm", "05/11/2015 05:00pm")
-	assert.Equal(t, result, 0);
+	_, error := Calculate("05/11/2015 5:00pm", "05/11/2015 05:00pm", "05/11/2015 05:00pm")
 	assert.Nil(t, error);
 }
 
 func TestShouldRequireAStartTime(t *testing.T) {
-	result, error := Calculate("", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
-	assert.Equal(t, result, 0);
+	_, error := Calculate("", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireAStartTimeToBeParsable(t *testing.T) {
-	result, error := Calculate("blah", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
-	assert.Equal(t, result, 0);
+	_, error := Calculate("blah", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireALeaveTime(t *testing.T) {
-	result, error := Calculate("05/11/2015 05:00pm", "", "05/11/2015 05:00pm");
-	assert.Equal(t, result, 0);
+	_, error := Calculate("05/11/2015 05:00pm", "", "05/11/2015 05:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireALeaveTimeToBeParsable(t *testing.T) {
-	result, error := Calculate("05/11/2015 05:00pm", "blah", "05/11/2015 05:00pm");
-	assert.Equal(t, result, 0);
+	_, error := Calculate("05/11/2015 05:00pm", "blah", "05/11/2015 05:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireABedTime(t *testing.T) {
-	result, error := Calculate("05/11/2015 05:00pm", "05/11/2015 05:00pm", "");
-	assert.Equal(t, result, 0);
+	_, error := Calculate("05/11/2015 05:00pm", "05/11/2015 05:00pm", "");
 	assert.NotNil(t, error);
 }
