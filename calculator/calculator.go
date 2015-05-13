b0127d043ac32f9e528293e1becd97cc74e18f85
Jerel R Hass <agies1@hotmail.com>
2015-05-12 23:30:08 -0400

    verify required bedtime

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -7,12 +7,15 @@ import (
 
 var utcLoc, err = time.LoadLocation("");
 
-func Calculate(startTime string, leaveTime string) (int, error) {
+func Calculate(startTime string, leaveTime string, bedTime string) (int, error) {
 	if (startTime == "") {
 		return 0, errors.New("Start time is required");
 	}
 	if (leaveTime == "") {
 		return 0, errors.New("Leave time is required");
+	}
+	if (bedTime == "") {
+		return 0, errors.New("Bed time is required");
 	}
 	return 0, nil;
 }
