5073010f3e1170ec0092282e10cbd7eb5706255d
Jerel R Hass <agies1@hotmail.com>
2015-05-14 00:06:50 -0400

    all tests passing

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,12 +1,19 @@
 package calculator
 
 import (
+	"math"
 	"time"
 	"errors"
 )
 
 var utcLoc, err = time.LoadLocation("");
 const dateFormat = "01/02/2006 3:04pm";
+var MinStartTime = 17;
+var MaxLeaveTime = 4;
+var BeforeBedtimePayment float64 = 12;
+var BedtimeToMidnightPayment float64 = 8;
+var AfterMidnightPayment float64 = 16;
+
 func Calculate(startTime string, leaveTime string, bedTime string) (int, error) {
 	if (startTime == "") {
 		return 0, errors.New("Start time is required");
@@ -18,20 +25,48 @@ func Calculate(startTime string, leaveTime string, bedTime string) (int, error) 
 	if (leaveTime == "") {
 		return 0, errors.New("Leave time is required");
 	}
-	_, error = time.Parse(dateFormat, leaveTime);
+	parsedLeaveTime, error := time.Parse(dateFormat, leaveTime);
 	if (error != nil) {
 		return 0, error;
 	}
 	if (bedTime == "") {
 		return 0, errors.New("Bed time is required");
 	}
-	_, error = time.Parse(dateFormat, bedTime);
+	parsedBedTime, error := time.Parse(dateFormat, bedTime);
 	if (error != nil) {
 		return 0, error;
 	}
 	year, month, day := parsedStartTime.Date();
-	if (parsedStartTime.Before(time.Date(year,month,day,17,0,0,0,utcLoc))) {
+	if (parsedStartTime.Before(time.Date(year,month,day,MinStartTime,0,0,0,utcLoc))) {
 		return 0, errors.New("Start time must be no earlier than 5PM");
 	}
-	return 0, nil;
+	if (parsedLeaveTime.After(time.Date(year,month,day,MaxLeaveTime,0,0,0,utcLoc).AddDate(0,0,1))) {
+		return 0, errors.New("Leave time must be no later than 4AM");
+	}
+	
+	var midnight = time.Date(year,month,day,0,0,0,0,utcLoc).AddDate(0,0,1);
+	
+	beforeBedTime := math.Floor(parsedBedTime.Sub(parsedStartTime).Hours());
+	var beforeBedTimeAmount float64 = 0.0;
+	if beforeBedTime > 0 { 
+		beforeBedTimeAmount = beforeBedTime * BeforeBedtimePayment; 
+	} 
+	
+	var timeToUse = parsedLeaveTime;
+	if midnight.Before(parsedLeaveTime) {
+		timeToUse = midnight;
+	}
+	afterBedTimeBeforeMidnight := math.Floor(timeToUse.Sub(parsedBedTime).Hours());
+	var afterBedTimeBeforeMidnightAmount float64 = 0.0;
+	if afterBedTimeBeforeMidnight > 0 { 
+		afterBedTimeBeforeMidnightAmount = afterBedTimeBeforeMidnight * BedtimeToMidnightPayment; 
+	} 
+	
+	afterMidnight := math.Floor(parsedLeaveTime.Sub(midnight).Hours());
+	var afterMidnightAmount float64 = 0.0;
+	if afterMidnight > 0 { 
+		afterMidnightAmount = afterMidnight * AfterMidnightPayment; 
+	} 
+	
+	return int(beforeBedTimeAmount + afterMidnightAmount + afterBedTimeBeforeMidnightAmount), nil;
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,41 +6,90 @@ import (
 )
 
 func TestCalculate(t *testing.T) {
-	_, error := Calculate("05/11/2015 5:00pm", "05/11/2015 05:00pm", "05/11/2015 05:00pm")
+	_, error := Calculate("05/11/2015 5:00pm", "05/11/2015 5:00pm", "05/11/2015 5:00pm")
 	assert.Nil(t, error);
 }
 
 func TestShouldRequireAStartTime(t *testing.T) {
-	_, error := Calculate("", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
-	assert.NotNil(t, error);
+	_, error := Calculate("", "05/11/2015 5:00pm", "05/11/2015 5:00pm");
+	assert.NotNil(t, error, error);
 }
 
 func TestShouldRequireAStartTimeToBeParsable(t *testing.T) {
-	_, error := Calculate("blah", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
+	_, error := Calculate("blah", "05/11/2015 5:00pm", "05/11/2015 5:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireALeaveTime(t *testing.T) {
-	_, error := Calculate("05/11/2015 05:00pm", "", "05/11/2015 05:00pm");
+	_, error := Calculate("05/11/2015 5:00pm", "", "05/11/2015 5:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireALeaveTimeToBeParsable(t *testing.T) {
-	_, error := Calculate("05/11/2015 05:00pm", "blah", "05/11/2015 05:00pm");
+	_, error := Calculate("05/11/2015 5:00pm", "blah", "05/11/2015 5:00pm");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireABedTime(t *testing.T) {
-	_, error := Calculate("05/11/2015 05:00pm", "05/11/2015 05:00pm", "");
+	_, error := Calculate("05/11/2015 5:00pm", "05/11/2015 5:00pm", "");
 	assert.NotNil(t, error);
 }
 
 func TestShouldRequireABedTimeToBeParsable(t *testing.T) {
-	_, error := Calculate("05/11/2015 05:00pm", "05/11/2015 05:00pm", "blah");
+	_, error := Calculate("05/11/2015 5:00pm", "05/11/2015 5:00pm", "blah");
 	assert.NotNil(t, error);
 }
 
-func TestShouldRequireAStartTimeOfNoEarlierThan5PM(t *testing.T) {
-	_, error := Calculate("05/11/2015 04:00pm", "05/11/2015 05:00pm", "05/11/2015 05:00pm");
+func TestShouldRequireAStartTimeOfNoEarlierThan5pm(t *testing.T) {
+	_, error := Calculate("05/11/2015 04:00pm", "05/11/2015 5:00pm", "05/11/2015 5:00pm");
 	assert.NotNil(t, error);
+}
+
+func TestShouldrequirealeavetimetobenolaterthan4amthefollowingday(t*testing.T){
+     _, error := Calculate("05/11/2015 5:00pm", "05/12/2015 5:00am", "05/11/2015 8:00pm");
+	 assert.NotNil(t, error);
+}
+func TestShouldrequireabedtimetobeparsable(t *testing.T){
+	_, error := Calculate("05/11/2015 5:00pm", "05/11/2015 4:00pm", "blah");        
+	assert.NotNil(t, error);
+}
+func TestShouldpay36beforebedfor3hours(t *testing.T){
+	amount, error := Calculate("05/11/2015 5:00pm", "05/11/2015 8:00pm", "05/11/2015 8:00pm");
+	assert.Equal(t, amount, 36);
+	assert.Nil(t, error);
+}
+func TestShouldpay12hrbeforebedequaling48dollarif4hrs(t *testing.T){
+	amount, error := Calculate("05/11/2015 5:00pm", "05/11/2015 9:00pm", "05/11/2015 9:00pm");
+	assert.Equal(t, amount, 48);
+	assert.Nil(t, error);
+} 
+func TestShouldpay0prebedtimeifstarttimeafterbedtime(t *testing.T){
+	BeforeBedtimePayment = 12;
+	amount, error := Calculate("05/11/2015 11:00pm", "05/11/2015 8:00pm", "05/11/2015 8:00pm");
+	assert.Equal(t, amount, 0);
+	assert.Nil(t, error);
+} 
+func TestShouldpay32afterbedandbeforemidnightif4hrs(t *testing.T){
+	BeforeBedtimePayment = 12;
+	amount, error := Calculate("05/11/2015 11:00pm", "05/12/2015 12:00am", "05/11/2015 8:00pm");
+	assert.Equal(t, amount, 32);
+	assert.Nil(t, error);
+} 
+func TestShouldpay8hrafterbedandbeforemidnight(t *testing.T){
+	BeforeBedtimePayment = 12;
+	amount, error := Calculate("05/11/2015 11:00pm", "05/12/2015 12:00am", "05/11/2015 7:00pm");
+	assert.Equal(t, amount, 40);
+	assert.Nil(t, error);
+}
+func TestShouldpay16aftermidnightif1hr(t *testing.T){
+	BeforeBedtimePayment = 0;
+	amount, error := Calculate("05/11/2015 7:00pm", "05/12/2015 1:00am", "05/12/2015 12:00am");
+	assert.Equal(t, amount, 16);
+	assert.Nil(t, error);
+} 
+func TestShouldpayonlyforwholehours(t *testing.T){
+	BeforeBedtimePayment = 12;
+	amount, error := Calculate("05/11/2015 5:15pm", "05/12/2015 1:45am", "05/11/2015 8:00pm");
+	assert.Equal(t, amount, 72);
+	assert.Nil(t, error);
 }
